bddf2f63e3dab1082321cccff175e07b6b8eb3a3
khbekzod <khabibullaevbekzodk@gmail.com>
2025-07-27 04:58:42 +0000

    Homework4-added

---
+++ Homework4/variable.tf
@@ -0,0 +1,53 @@
+variable vpc {
+    type = object({
+      cidr = string
+    })
+    default = {
+      cidr = ""
+    }
+}
+
+variable subnet {
+  type = list(object({
+    cidr = string
+    az = string
+    name = string
+}))
+  default = [
+    {
+    cidr = ""
+    az = ""
+    name = ""
+  },
+      {
+    cidr = ""
+    az = ""
+    name = ""
+  },
+      {
+    cidr = ""
+    az = ""
+    name = ""
+  },
+     {
+    cidr = ""
+    az = ""
+    name = ""
+      },
+  ]
+}
+
+variable igw_name {
+  type = string
+  default = ""
+}
+
+variable rt {
+    type = list(string)
+    default = []
+}
+
+variable port {
+  type = list(number)
+  default = []
+}
